d47108921b7a4e2bb53bc47c890193b2d0a83d5a
wangxinglin <wangxinglin@uenpay.com>
2025-04-29 15:18:05 +0800

    init

---
+++ src/test/java/com/xaiver/emotion/EmotionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xaiver.emotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
